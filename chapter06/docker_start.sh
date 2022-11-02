bbb49d3965fad86ed9e0de8b61b310145e1d341b
길벗 IT전문서 <6995518+gilbutITbook@users.noreply.github.com>
2022-11-02 16:34:38 +0900

    Delete chapter06 directory

--- chapter06/docker_start.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --rm -p 18080:18080 hotel-api:latest
--- chapter06/src/main/java/com/springtour/example/chapter06/domain/email/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.springtour.example.chapter06.domain.email;
-
-public interface EmailService {
-
-    boolean sendEmail(EmailAddress emailAddress);
-}
